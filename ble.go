3c53799a7b9f6ee98a4842972ba2b6dcd009cafb
Lorenz Koestler <lorenz@koestler.ch>
2023-11-30 14:32:40 +0100

    add x/bluetooth and implement scanning

--- ble.go
+++ ble.go
@@ -2,7 +2,10 @@ package ble
 
 import (
 	"log"
+	"tinygo.org/x/bluetooth"
 )
+
+var adapter = bluetooth.DefaultAdapter
 
 type BleStruct struct {
 	cfg Config
@@ -11,6 +14,21 @@ type BleStruct struct {
 func New(cfg Config) (*BleStruct, error) {
 	if cfg.LogDebug() {
 		log.Printf("ble[%s]: create", cfg.Name())
+	}
+
+	if err := adapter.Enable(); err != nil {
+		log.Printf("ble[%s]: error during enable: %s", cfg.Name(), err)
+	}
+
+	// Start scanning.
+	log.Printf("ble[%s]: start scanning", cfg.Name())
+	{
+		err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+			log.Printf("ble[%s]: found device: %s %d %s", cfg.Name(), device.Address.String(), device.RSSI, device.LocalName())
+		})
+		if err != nil {
+			log.Printf("ble[%s]: error during scanning: %s", cfg.Name(), err)
+		}
 	}
 
 	return &BleStruct{
