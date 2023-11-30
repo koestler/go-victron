2720f3e4ec0e4bbda63b85d30dd10d9824f51c60
Lorenz Koestler <lorenz@koestler.ch>
2023-11-30 18:37:31 +0100

    start directly using muka/go-bluetooth

--- ble.go
+++ ble.go
@@ -1,11 +1,14 @@
 package ble
 
 import (
+	"context"
+	"fmt"
+	"github.com/muka/go-bluetooth/api"
+	"github.com/muka/go-bluetooth/bluez/profile/adapter"
+	"github.com/muka/go-bluetooth/bluez/profile/device"
 	"log"
-	"tinygo.org/x/bluetooth"
+	"strings"
 )
-
-var adapter = bluetooth.DefaultAdapter
 
 type Config interface {
 	Name() string
@@ -19,49 +22,129 @@ type DeviceConfig interface {
 }
 
 type BleStruct struct {
-	cfg Config
+	cfg    Config
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 func New(cfg Config) (*BleStruct, error) {
-	if cfg.LogDebug() {
-		log.Printf("ble[%s]: create", cfg.Name())
-	}
+	ctx, cancel := context.WithCancel(context.Background())
 
 	ble := &BleStruct{
-		cfg: cfg,
+		cfg:    cfg,
+		ctx:    ctx,
+		cancel: cancel,
 	}
 
+	adapterID := "hci0"
+
 	go func() {
-		if err := adapter.Enable(); err != nil {
-			log.Printf("ble[%s]: error during enable: %s", cfg.Name(), err)
+		//clean up connection on exit
+		defer api.Exit()
+
+		if cfg.LogDebug() {
+			log.Printf("ble[%s]: get adapterId=%s", ble.Name(), adapterID)
+		}
+		a, err := adapter.GetAdapter(adapterID)
+		if err != nil {
+			log.Printf("ble[%s]: error while getting adapter: %s", ble.Name(), err)
+			return
 		}
 
-		// Start scanning.
-		log.Printf("ble[%s]: start scanning", cfg.Name())
-
-		if err := adapter.Scan(func(a *bluetooth.Adapter, result bluetooth.ScanResult) {
-			ble.bleAdvHandler(a, result)
-		}); err != nil {
-			log.Printf("ble[%s]: error during scanning: %s", cfg.Name(), err)
+		if cfg.LogDebug() {
+			log.Printf("ble[%s]: flush devices", ble.Name())
 		}
+		err = a.FlushDevices()
+		if err != nil {
+			log.Printf("ble[%s]: error during flush: %s", ble.Name(), err)
+			return
+		}
+
+		if cfg.LogDebug() {
+			log.Printf("ble[%s]: start discovery", ble.Name())
+		}
+
+		discovery, cancel, err := api.Discover(a, nil)
+		if err != nil {
+			log.Printf("ble[%s]: error during discovery: %s", ble.Name(), err)
+		}
+		defer cancel()
+
+		go func() {
+			for ev := range discovery {
+				if ev.Type == adapter.DeviceRemoved {
+					continue
+				}
+
+				dev, err := device.NewDevice1(ev.Path)
+				if err != nil {
+					log.Printf("ble[%s]: %s: %s", ble.Name(), ev.Path, err)
+					continue
+				}
+
+				if !ble.getDevice(dev.Properties.Address) {
+					continue
+				}
+
+				log.Printf("ble[%s] device recovered: %s", ble.Name(), ev.Path)
+
+				if dev == nil {
+					log.Printf("ble[%s]: device %s not found", ble.Name(), ev.Path)
+					continue
+				}
+
+				log.Printf("ble[%s]: name=%s addr=%s rssi=%d", ble.Name(), dev.Properties.Name, dev.Properties.Address, dev.Properties.RSSI)
+
+				log.Printf("ble[%s]: addr=%s manufacturerData=%x", ble.Name(), dev.Properties.Address, dev.Properties.ManufacturerData)
+
+				go func(ev *adapter.DeviceDiscovered) {
+					err = connectDevice(dev)
+					if err != nil {
+						log.Printf("ble[%s]: beacon %s failed: %s", ble.Name(), ev.Path, err)
+					}
+				}(ev)
+			}
+		}()
+
+		// wait for cancel
+		<-ble.ctx.Done()
 	}()
 
 	return ble, nil
 }
 
-func (ble *BleStruct) bleAdvHandler(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-	if device.LocalName() != "SmartSolar HQ19499RHC5" {
-		return
+func connectDevice(dev *device.Device1) error {
+	log.Printf("")
+
+	propUpdates, err := dev.WatchProperties()
+	if err != nil {
+		return fmt.Errorf("cannot watch props: %s", err)
 	}
 
-	log.Printf("ble[%s]: found device: %s %d %s", ble.Name(), device.Address.String(), device.RSSI, device.LocalName())
-	log.Printf("ble[%s]: address: %x, isRandom=%v", ble.Name(), device.Address.String(), device.Address.IsRandom())
-	// log.Printf("ble[%s]: bytes received: %x", ble.Name(), device.Bytes())
-	// log.Printf("ble[%s]: ManufacturerData: %#v", ble.Name(), device.ManufacturerData())
+	for pu := range propUpdates {
+		log.Printf("dev=%s, received prop update: %#v", dev.Properties.Name, pu)
+	}
+
+	if err := dev.UnwatchProperties(propUpdates); err != nil {
+		log.Printf("error during unwatch: %s", err)
+	}
+
+	return nil
 }
 
-func (ble *BleStruct) getDevice() {
+func (ble *BleStruct) getDevice(bluezAddr string) bool {
+	for _, d := range ble.cfg.Devices() {
+		if d.MacAddress() == bluezAddrToOurAddr(bluezAddr) {
+			return true
+		}
+	}
+	return false
+}
 
+// input: D4:9D:D2:92:62:02
+// output d49dd2926202
+func bluezAddrToOurAddr(i string) string {
+	return strings.ToLower(strings.ReplaceAll(i, ":", ""))
 }
 
 func (ble *BleStruct) Name() string {
@@ -69,5 +152,5 @@ func (ble *BleStruct) Name() string {
 }
 
 func (ble *BleStruct) Shutdown() {
-
+	ble.cancel()
 }
