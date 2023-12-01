792d2df1230a7d44743b5f542536873753062ac9
Lorenz Koestler <lorenz@koestler.ch>
2023-12-01 12:58:49 +0100

    refactor product definitions to seperate package

--- registerFactory.go
+++ registerFactory.go
@@ -1,296 +1,296 @@
 package victronDevice
 
 import (
-	"github.com/koestler/go-iotdevice/vedirect"
+	vd "github.com/koestler/go-iotdevice/victronDefinitions"
 )
 
-func RegisterFactoryByProduct(product vedirect.VeProduct) []VictronRegister {
+func RegisterFactoryByProduct(product vd.VeProduct) []VictronRegister {
 	switch product {
-	case vedirect.VeProductBMV700:
+	case vd.VeProductBMV700:
 		return RegisterListBmv700
-	case vedirect.VeProductBMV702:
+	case vd.VeProductBMV702:
 		return RegisterListBmv702
-	case vedirect.VeProductBMV700H:
+	case vd.VeProductBMV700H:
 		return RegisterListBmv700
-	case vedirect.VeProductBlueSolarMPPT70_15:
+	case vd.VeProductBlueSolarMPPT70_15:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT75_50:
+	case vd.VeProductBlueSolarMPPT75_50:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_35:
+	case vd.VeProductBlueSolarMPPT150_35:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT75_15:
+	case vd.VeProductBlueSolarMPPT75_15:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_15:
+	case vd.VeProductBlueSolarMPPT100_15:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_30:
+	case vd.VeProductBlueSolarMPPT100_30:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_50:
+	case vd.VeProductBlueSolarMPPT100_50:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_70:
+	case vd.VeProductBlueSolarMPPT150_70:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_100:
+	case vd.VeProductBlueSolarMPPT150_100:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_50rev2:
+	case vd.VeProductBlueSolarMPPT100_50rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_30rev2:
+	case vd.VeProductBlueSolarMPPT100_30rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_35rev2:
+	case vd.VeProductBlueSolarMPPT150_35rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT75_10:
+	case vd.VeProductBlueSolarMPPT75_10:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_45:
+	case vd.VeProductBlueSolarMPPT150_45:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_60:
+	case vd.VeProductBlueSolarMPPT150_60:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_85:
+	case vd.VeProductBlueSolarMPPT150_85:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_100:
+	case vd.VeProductSmartSolarMPPT250_100:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_100:
+	case vd.VeProductSmartSolarMPPT150_100:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_85:
+	case vd.VeProductSmartSolarMPPT150_85:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT75_15:
+	case vd.VeProductSmartSolarMPPT75_15:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT75_10:
+	case vd.VeProductSmartSolarMPPT75_10:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT100_15:
+	case vd.VeProductSmartSolarMPPT100_15:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT100_30:
+	case vd.VeProductSmartSolarMPPT100_30:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT100_50:
+	case vd.VeProductSmartSolarMPPT100_50:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_35:
+	case vd.VeProductSmartSolarMPPT150_35:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_100rev2:
+	case vd.VeProductSmartSolarMPPT150_100rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_85rev2:
+	case vd.VeProductSmartSolarMPPT150_85rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_70:
+	case vd.VeProductSmartSolarMPPT250_70:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_85:
+	case vd.VeProductSmartSolarMPPT250_85:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_60:
+	case vd.VeProductSmartSolarMPPT250_60:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_45:
+	case vd.VeProductSmartSolarMPPT250_45:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT100_20:
+	case vd.VeProductSmartSolarMPPT100_20:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT100_2048V:
+	case vd.VeProductSmartSolarMPPT100_2048V:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_45:
+	case vd.VeProductSmartSolarMPPT150_45:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_60:
+	case vd.VeProductSmartSolarMPPT150_60:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_70:
+	case vd.VeProductSmartSolarMPPT150_70:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_85rev2:
+	case vd.VeProductSmartSolarMPPT250_85rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_100rev2:
+	case vd.VeProductSmartSolarMPPT250_100rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_20:
+	case vd.VeProductBlueSolarMPPT100_20:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_2048V:
+	case vd.VeProductBlueSolarMPPT100_2048V:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_60rev2:
+	case vd.VeProductSmartSolarMPPT250_60rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT250_70rev2:
+	case vd.VeProductSmartSolarMPPT250_70rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_45rev2:
+	case vd.VeProductSmartSolarMPPT150_45rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_60rev2:
+	case vd.VeProductSmartSolarMPPT150_60rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_70rev2:
+	case vd.VeProductSmartSolarMPPT150_70rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_85rev3:
+	case vd.VeProductSmartSolarMPPT150_85rev3:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT150_100rev3:
+	case vd.VeProductSmartSolarMPPT150_100rev3:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_45rev2:
+	case vd.VeProductBlueSolarMPPT150_45rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_60rev2:
+	case vd.VeProductBlueSolarMPPT150_60rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_70rev2:
+	case vd.VeProductBlueSolarMPPT150_70rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_45rev3:
+	case vd.VeProductBlueSolarMPPT150_45rev3:
 		return RegisterListSolar
-	case vedirect.VeProductSmatrtSolarMPPT150_45rev3:
+	case vd.VeProductSmatrtSolarMPPT150_45rev3:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT70_10rev2:
+	case vd.VeProductSmartSolarMPPT70_10rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPT75_15rev2:
+	case vd.VeProductSmartSolarMPPT75_15rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_30rev3:
+	case vd.VeProductBlueSolarMPPT100_30rev3:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_50rev3:
+	case vd.VeProductBlueSolarMPPT100_50rev3:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT150_35rev3:
+	case vd.VeProductBlueSolarMPPT150_35rev3:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT75_10rev2:
+	case vd.VeProductBlueSolarMPPT75_10rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT75_15rev2:
+	case vd.VeProductBlueSolarMPPT75_15rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPT100_15rev2:
+	case vd.VeProductBlueSolarMPPT100_15rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan150_70:
+	case vd.VeProductSmartSolarMPPTVECan150_70:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan150_45:
+	case vd.VeProductSmartSolarMPPTVECan150_45:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan150_60:
+	case vd.VeProductSmartSolarMPPTVECan150_60:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan150_85:
+	case vd.VeProductSmartSolarMPPTVECan150_85:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan150_100:
+	case vd.VeProductSmartSolarMPPTVECan150_100:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan250_45:
+	case vd.VeProductSmartSolarMPPTVECan250_45:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan250_60:
+	case vd.VeProductSmartSolarMPPTVECan250_60:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan250_70:
+	case vd.VeProductSmartSolarMPPTVECan250_70:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan250_85:
+	case vd.VeProductSmartSolarMPPTVECan250_85:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan250_100:
+	case vd.VeProductSmartSolarMPPTVECan250_100:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan150_70rev2:
+	case vd.VeProductSmartSolarMPPTVECan150_70rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan150_85rev2:
+	case vd.VeProductSmartSolarMPPTVECan150_85rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan150_100rev2:
+	case vd.VeProductSmartSolarMPPTVECan150_100rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPTVECan150_100:
+	case vd.VeProductBlueSolarMPPTVECan150_100:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPTVECan250_70:
+	case vd.VeProductBlueSolarMPPTVECan250_70:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPTVECan250_100:
+	case vd.VeProductBlueSolarMPPTVECan250_100:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan250_70rev2:
+	case vd.VeProductSmartSolarMPPTVECan250_70rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan250_100rev2:
+	case vd.VeProductSmartSolarMPPTVECan250_100rev2:
 		return RegisterListSolar
-	case vedirect.VeProductSmartSolarMPPTVECan250_85rev2:
+	case vd.VeProductSmartSolarMPPTVECan250_85rev2:
 		return RegisterListSolar
-	case vedirect.VeProductBlueSolarMPPTVECan150_100rev2:
+	case vd.VeProductBlueSolarMPPTVECan150_100rev2:
 		return RegisterListSolar
-	case vedirect.VeProductPhoenixInverter12V250VA230V:
+	case vd.VeProductPhoenixInverter12V250VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V250VA230V:
+	case vd.VeProductPhoenixInverter24V250VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V250VA230V:
+	case vd.VeProductPhoenixInverter48V250VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V250VA120V:
+	case vd.VeProductPhoenixInverter12V250VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V250VA120V:
+	case vd.VeProductPhoenixInverter24V250VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V250VA120V:
+	case vd.VeProductPhoenixInverter48V250VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V375VA230V:
+	case vd.VeProductPhoenixInverter12V375VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V375VA230V:
+	case vd.VeProductPhoenixInverter24V375VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V375VA230V:
+	case vd.VeProductPhoenixInverter48V375VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V375VA120V:
+	case vd.VeProductPhoenixInverter12V375VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V375VA120V:
+	case vd.VeProductPhoenixInverter24V375VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V375VA120V:
+	case vd.VeProductPhoenixInverter48V375VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V500VA230V:
+	case vd.VeProductPhoenixInverter12V500VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V500VA230V:
+	case vd.VeProductPhoenixInverter24V500VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V500VA230V:
+	case vd.VeProductPhoenixInverter48V500VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V500VA120V:
+	case vd.VeProductPhoenixInverter12V500VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V500VA120V:
+	case vd.VeProductPhoenixInverter24V500VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V500VA120V:
+	case vd.VeProductPhoenixInverter48V500VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V800VA230V:
+	case vd.VeProductPhoenixInverter12V800VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V800VA230V:
+	case vd.VeProductPhoenixInverter24V800VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V800VA230V:
+	case vd.VeProductPhoenixInverter48V800VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V800VA120V:
+	case vd.VeProductPhoenixInverter12V800VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V800VA120V:
+	case vd.VeProductPhoenixInverter24V800VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V800VA120V:
+	case vd.VeProductPhoenixInverter48V800VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V1200VA230V:
+	case vd.VeProductPhoenixInverter12V1200VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V1200VA230V:
+	case vd.VeProductPhoenixInverter24V1200VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V1200VA230V:
+	case vd.VeProductPhoenixInverter48V1200VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V1200VA120V:
+	case vd.VeProductPhoenixInverter12V1200VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V1200VA120V:
+	case vd.VeProductPhoenixInverter24V1200VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V1200VA120V:
+	case vd.VeProductPhoenixInverter48V1200VA120V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V1600VA230V:
+	case vd.VeProductPhoenixInverter12V1600VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V1600VA230V:
+	case vd.VeProductPhoenixInverter24V1600VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V1600VA230V:
+	case vd.VeProductPhoenixInverter48V1600VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V2000VA230V:
+	case vd.VeProductPhoenixInverter12V2000VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V2000VA230V:
+	case vd.VeProductPhoenixInverter24V2000VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V2000VA230V:
+	case vd.VeProductPhoenixInverter48V2000VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V3000VA230V:
+	case vd.VeProductPhoenixInverter12V3000VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V3000VA230V:
+	case vd.VeProductPhoenixInverter24V3000VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V3000VA230V:
+	case vd.VeProductPhoenixInverter48V3000VA230V:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverterSmart12V5000VA230Vac64k:
+	case vd.VeProductPhoenixInverterSmart12V5000VA230Vac64k:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverterSmart24V5000VA230Vac64k:
+	case vd.VeProductPhoenixInverterSmart24V5000VA230Vac64k:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverterSmart48V5000VA230Vac64k:
+	case vd.VeProductPhoenixInverterSmart48V5000VA230Vac64k:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V800VA230Vac64kHS:
+	case vd.VeProductPhoenixInverter12V800VA230Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V800VA230Vac64kHS:
+	case vd.VeProductPhoenixInverter24V800VA230Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V800VA230Vac64kHS:
+	case vd.VeProductPhoenixInverter48V800VA230Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V800VA120Vac64kHS:
+	case vd.VeProductPhoenixInverter12V800VA120Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V800VA120Vac64kHS:
+	case vd.VeProductPhoenixInverter24V800VA120Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V800VA120Vac64kHS:
+	case vd.VeProductPhoenixInverter48V800VA120Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V1200VA230Vac64kHS:
+	case vd.VeProductPhoenixInverter12V1200VA230Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V1200VA230Vac64kHS:
+	case vd.VeProductPhoenixInverter24V1200VA230Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V1200VA230Vac64kHS:
+	case vd.VeProductPhoenixInverter48V1200VA230Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter12V1200VA120Vac64kHS:
+	case vd.VeProductPhoenixInverter12V1200VA120Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter24V1200VA120Vac64kHS:
+	case vd.VeProductPhoenixInverter24V1200VA120Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductPhoenixInverter48V1200VA120Vac64kHS:
+	case vd.VeProductPhoenixInverter48V1200VA120Vac64kHS:
 		return RegisterListInverter
-	case vedirect.VeProductBMV712Smart:
+	case vd.VeProductBMV712Smart:
 		return RegisterListBmv712
-	case vedirect.VeProductBMV710HSmart:
+	case vd.VeProductBMV710HSmart:
 		return RegisterListBmv712
-	case vedirect.VeProductBMV712SmartRev2:
+	case vd.VeProductBMV712SmartRev2:
 		return RegisterListBmv712
-	case vedirect.VeProductSmartShunt500A_50mV:
+	case vd.VeProductSmartShunt500A_50mV:
 		return RegisterListBmv712
-	case vedirect.VeProductSmartShunt1000A_50mV:
+	case vd.VeProductSmartShunt1000A_50mV:
 		return RegisterListBmv712
-	case vedirect.VeProductSmartShunt2000A_50mV:
+	case vd.VeProductSmartShunt2000A_50mV:
 		return RegisterListBmv712
 	}
 	return nil
