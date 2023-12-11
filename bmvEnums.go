849fbe8b73fee0dfc0ffa77fe28bef0d1c15f7ca
Lorenz Koestler <lorenz@koestler.ch>
2023-12-11 18:43:41 +0100

    implement ble battery monitor parsing

---
+++ bmvEnums.go
@@ -0,0 +1,27 @@
+package victronDefinitions
+
+type BmvAuxMode uint8
+
+const (
+	BmvAuxModeStartervoltage  BmvAuxMode = 0
+	BmvAuxModeMidpointVoltage BmvAuxMode = 1
+	BmvAuxModeTemperature     BmvAuxMode = 2
+	BmvAuxModeDisabled        BmvAuxMode = 3
+)
+
+func GetBmvAuxModeMap() map[BmvAuxMode]string {
+	return map[BmvAuxMode]string{
+		BmvAuxModeStartervoltage:  "Starter voltage",
+		BmvAuxModeMidpointVoltage: "Mid-point voltage",
+		BmvAuxModeTemperature:     "Temperature",
+		BmvAuxModeDisabled:        "Disabled",
+	}
+}
+
+func (s BmvAuxMode) String() string {
+	m := GetBmvAuxModeMap()
+	if v, ok := m[s]; ok {
+		return v
+	}
+	return ""
+}
