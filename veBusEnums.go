1c969b02dde494bc7ade9a6392bb88db1e573961
Lorenz Koestler <lorenz@koestler.ch>
2023-12-13 22:42:18 +0100

    implement ve.bus decoding

---
+++ veBusEnums.go
@@ -0,0 +1,27 @@
+package victronDefinitions
+
+type VeBusAlarm uint8
+
+const (
+	VeBusAlarmNoAlarm   VeBusAlarm = 0
+	VeBusAlarmWarning   VeBusAlarm = 1
+	VeBusAlarmAlarm     VeBusAlarm = 2
+	VeBusAlarmUndefined VeBusAlarm = 3
+)
+
+func GetVeBusAlarmMap() map[VeBusAlarm]string {
+	return map[VeBusAlarm]string{
+		VeBusAlarmNoAlarm:   "No Alarm",
+		VeBusAlarmWarning:   "Warning",
+		VeBusAlarmAlarm:     "Alarm",
+		VeBusAlarmUndefined: "Undefined",
+	}
+}
+
+func (s VeBusAlarm) String() string {
+	m := GetVeBusAlarmMap()
+	if v, ok := m[s]; ok {
+		return v
+	}
+	return ""
+}
