8246723bc3edcb3d15c8429f03e0c5f161ae0ffd
Lorenz Koestler <lorenz@koestler.ch>
2023-12-13 18:12:23 +0100

    fix gxDevice decoding, implement ac charger decoding

--- gxDevice.go
+++ gxDevice.go
@@ -33,7 +33,7 @@ func DecodeGxDeviceRecord(inp []byte) (ret GxDeviceRecord, err error) {
 	} else {
 		ret.BatteryVoltage = math.NaN()
 	}
-	if v := binary.LittleEndian.Uint32(inp[2:6]) & 0x0FFFFF  ; v != 0x0FFFFF {
+	if v := binary.LittleEndian.Uint32(inp[2:6]) & 0x0FFFFF; v != 0x0FFFFF {
 		ret.PvPower = float64(v)
 	} else {
 		ret.PvPower = math.NaN()
@@ -43,7 +43,7 @@ func DecodeGxDeviceRecord(inp []byte) (ret GxDeviceRecord, err error) {
 	} else {
 		ret.Soc = math.NaN()
 	}
-	if v := (binary.LittleEndian.Uint32(inp[5:9]) >> 3) & 0x; v != 0x0FFFFF {
+	if v := (binary.LittleEndian.Uint32(inp[5:9]) >> 3) & 0x0FFFFF; v != 0x0FFFFF {
 		ret.BatteryPower = float64(int32(v))
 	} else {
 		ret.BatteryPower = math.NaN()
