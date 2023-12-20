300fe707b35448486702fb34673fd99128a01cb5
Lorenz Koestler <lorenz@koestler.ch>
2023-12-20 23:59:45 +0100

    add test cases for invalid address

--- vedirect/vedirect_test.go
+++ vedirect/vedirect_test.go
@@ -2,6 +2,7 @@ package vedirect_test
 
 import (
 	"bytes"
+	"errors"
 	"github.com/koestler/go-victron/vedirect"
 	"log"
 	"testing"
@@ -19,6 +20,7 @@ func TestVedirect(t *testing.T) {
 		":7D0ED0091\n": "\r\nPID\t0xA056\r\nFW\t161\r\nSER#\tHQ19499RHC5\r\nV\t11880\r\nI\t0\r\n" +
 			"VPV\t10\r\nPPV\t0\r\nCS\t0\r\nMPPT\t0\r\nOR\t0x00000001\r\nERR\t0\r\nLOAD\tON\r\nH19" +
 			"\t4803\r\nH20\t0\r\nH21\t0\r\nH22\t0\r\nH23\t0\r\nHSDS\t280\r\nChecksum\t):7D0ED00000091\n", // GetUint(0xEDD0) = 0
+		":777770060\n": ":77777015F\n", // Get{Uint, Int, String}(0x7777) -> unknown id
 	})
 	defer io.CheckEverythingHeard()
 	defer io.CheckClosed()
@@ -83,6 +85,17 @@ func TestVedirect(t *testing.T) {
 		t.Errorf("cannot get 0xEDD0: %v", err)
 	} else if expect := uint64(0); expect != got {
 		t.Errorf("fetching 0xEDD0: expected 0x%x but 0x%x", expect, got)
+	}
+
+	// Get{Uint, Int, String}(0x7777) -> unknown id
+	if _, err := vd.GetUint(0x7777); !errors.Is(err, vedirect.ErrUnknownId) {
+		t.Errorf("GetUInt(invalid address): expected ErrUnknownId but got: %v", err)
+	}
+	if _, err := vd.GetInt(0x7777); !errors.Is(err, vedirect.ErrUnknownId) {
+		t.Errorf("GetInt(invalid address): expected ErrUnknownId but got: %v", err)
+	}
+	if _, err := vd.GetString(0x7777); !errors.Is(err, vedirect.ErrUnknownId) {
+		t.Errorf("GetString(invalid address): expected ErrUnknownId but got: %v", err)
 	}
 }
 
