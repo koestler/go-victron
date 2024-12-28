8f1195c9e3227158a0d84ee57ea45c8a998ea9fa
Lorenz Koestler <lorenz@koestler.ch>
2024-12-28 10:30:51 +0100

    add RecordType enum

---
+++ veconst/ble.go
@@ -0,0 +1,4 @@
+package veconst
+
+// BleManufacturerId sent by Victron devices in the Bluetooth advertisement packets in the manufacturer data.
+const BleManufacturerId = 0x2e1
--- veconst/bleConstants.go
+++
@@ -1,4 +0,0 @@
-package veconst
-
-// BleManufacturerId is sent in Bluetooth advertisement packets in the manufacturer data.
-const BleManufacturerId = 0x2e1
