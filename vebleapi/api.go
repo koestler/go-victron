c01e0794f34773fd84f04de4195833daef6e2897
Lorenz Koestler <lorenz@koestler.ch>
2025-01-04 20:19:40 +0100

    bugfix

--- vebleapi/api.go
+++ vebleapi/api.go
@@ -29,41 +29,49 @@ func (a *Api) StreamRegisters(ctx context.Context, h func(rssi int, registers ve
 	var lastMD []byte
 
 	listener := a.adapter.RegisterNameListener(a.name)
-	// run until ctx is done
+	defer listener.End()
+
+	// terminate when ctx is done
 	go func() {
 		<-ctx.Done()
 		listener.End()
 	}()
 
-	for p := range listener.Drain() {
-		// ignore duplicate packets
-		md := p.ManufacturerData()
-
-		if bytes.Equal(lastMD, md) {
-			continue
-		}
-		lastMD = md
-
-		ef, err := veble.DecodeFrame(md, a.logger)
-		if err != nil {
-			fmt.Println("error decoding frame:", err)
+	for {
+		select {
+		case <-ctx.Done():
 			return
+		case p := <-listener.Drain():
+			// ignore duplicate packets
+			md := p.ManufacturerData()
+
+			if bytes.Equal(lastMD, md) {
+				continue
+			}
+			lastMD = md
+
+			ef, err := veble.DecodeFrame(md, a.logger)
+			if err != nil {
+				fmt.Println("error decoding frame:", err)
+				return
+			}
+
+			fmt.Printf("received packet from address=%s, RSSI=%d, name=%s, IV=%d\n", p.Name(), p.RSSI(), p.Name(), ef.IV)
+
+			df, err := veble.DecryptFrame(ef, a.encryptionKey, a.logger)
+			if err != nil {
+				fmt.Println("error decrypting frame:", err)
+				return
+			}
+
+			registers, err := veblerecord.Decode(df.RecordType, df.DecryptedBytes)
+			if err != nil {
+				fmt.Println("error decoding registers:", err)
+				return
+			}
+
+			h(p.RSSI(), registers)
 		}
 
-		fmt.Printf("received packet from address=%s, RSSI=%d, name=%s, IV=%d\n", p.Name(), p.RSSI(), p.Name(), ef.IV)
-
-		df, err := veble.DecryptFrame(ef, a.encryptionKey, a.logger)
-		if err != nil {
-			fmt.Println("error decrypting frame:", err)
-			return
-		}
-
-		registers, err := veblerecord.Decode(df.RecordType, df.DecryptedBytes)
-		if err != nil {
-			fmt.Println("error decoding registers:", err)
-			return
-		}
-
-		h(p.RSSI(), registers)
 	}
 }
